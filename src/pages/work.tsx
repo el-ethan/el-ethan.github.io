0c45e4dfd188faaf09c5d51d241d96781804e0ee
Ethan Skinner <ethan.skinner@backlotcars.com>
2020-09-13 15:05:41 -0500

    Add back bubble

--- src/pages/work.tsx
+++ src/pages/work.tsx
@@ -1,18 +1,14 @@
 import React from "react"
-import { Link } from "gatsby"
-import { css } from "@emotion/core"
 
 import { Column, Row } from "../components/Containers/Containers"
-import WorkBubble from "../components/Bubble/WorkBubble"
 import IconBubble from "../components/Bubble/IconBubble"
+import BackBubble from "../components/Bubble/BackBubble"
 
 const WorkPage = () => (
     <Column>
         <Row></Row>
         <Row>
-            <Link to="/">
-                <WorkBubble></WorkBubble>
-            </Link>
+            <BackBubble></BackBubble>
             <IconBubble icon="linkedin"></IconBubble>
             <IconBubble icon="soWork"></IconBubble>
         </Row>
