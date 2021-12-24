76b76516bd7da410fb1156953c3857d43f0a796c
Ethan Skinner <10249884+el-ethan@users.noreply.github.com>
2021-12-23 19:45:36 -0600

    Update metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
     siteMetadata: {
         title: `Ethan Skinner`,
-        description: `divs, spans and arrows by Ethan Skinner`,
+        description: `divs and spans by Ethan Skinner`,
         author: `el-ethan`,
     },
     plugins: [
@@ -16,7 +16,7 @@ module.exports = {
         `gatsby-transformer-sharp`,
         `gatsby-plugin-sharp`,
         {
-            resolve: "gatsby-plugin-page-transitions",
+            resolve: 'gatsby-plugin-page-transitions',
             options: {
                 transitionTime: 500,
             },
@@ -24,15 +24,15 @@ module.exports = {
         {
             resolve: `gatsby-plugin-manifest`,
             options: {
-              name: `Ethan Skinner`,
-              short_name: `Ethan`,
-              start_url: `/`,
-              background_color: `#292d3e`,
-              theme_color: `#292d3e`,
-              display: `standalone`,
-              icon: `src/images/favicon-16x16.png`, // This path is relative to the root of the site.
+                name: `💻`,
+                short_name: `Ethan`,
+                start_url: `/`,
+                background_color: `#292d3e`,
+                theme_color: `#292d3e`,
+                display: `standalone`,
+                icon: `src/images/favicon-16x16.png`, // This path is relative to the root of the site.
             },
-          },
+        },
 
         // this (optional) plugin enables Progressive Web App + Offline functionality
         // To learn more, visit: https://gatsby.dev/offline
