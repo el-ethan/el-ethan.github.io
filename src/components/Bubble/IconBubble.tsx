0eea279f6c28bb070d9621a6714cbfa970282c43
Ethan Skinner <10249884+el-ethan@users.noreply.github.com>
2022-02-12 13:35:51 -0600

    cleanup

--- src/components/Bubble/IconBubble.tsx
+++ src/components/Bubble/IconBubble.tsx
@@ -3,7 +3,6 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {
     faGithub,
     faStackOverflow,
-    faInstagram,
     faGoodreads,
     faLinkedin,
     IconDefinition,
@@ -21,9 +20,7 @@ interface IconBubbleConfig {
 const IconBubbleConfigs = {
     github: { icon: faGithub, link: 'https://github.com/el-ethan', color: '#c3e88d' },
     linkedin: { icon: faLinkedin, link: 'https://www.linkedin.com/in/ethan-e-skinner/', color: '#82AAFF' },
-    soWork: { icon: faStackOverflow, link: 'https://stackoverflow.com/story/eskinner', color: '#c792ea' },
     stackoverflow: { icon: faStackOverflow, link: 'https://stackoverflow.com/users/3642398/elethan?tab=profile', color: '#f07178' },
-    instagram: { icon: faInstagram, link: 'https://www.instagram.com/ethans_cup/', color: '#ffcb6b' },
     goodreads: { icon: faGoodreads, link: 'https://www.goodreads.com/user/show/75031232-ethan', color: '#c792ea' },
 }
 
@@ -37,7 +34,7 @@ const IconBubble = ({ icon }: IconBubbleProps) => {
     return (
         <div className={iconBubbleContainer}>
             <a className={bubble} href={config.link} aria-label={`Link to Ethan's ${icon}`} target="_blank">
-                {<FontAwesomeIcon color={config.color} icon={config.icon}></FontAwesomeIcon>}
+                <FontAwesomeIcon color={config.color} icon={config.icon}></FontAwesomeIcon>
             </a>
         </div>
     )
