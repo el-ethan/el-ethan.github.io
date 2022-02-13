7bc70cc426ec6bfccabd8f24e85af5e0a323f7a3
Ethan Skinner <10249884+el-ethan@users.noreply.github.com>
2022-02-13 09:34:02 -0600

    Remove unused import

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Column, Row } from '../components/Containers/Containers'
+import { Column } from '../components/Containers/Containers'
 
 const NotFoundPage = () => (
     <Column>
