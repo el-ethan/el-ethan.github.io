437ac97e63e0e1e2c54e3a3382e7f0cfb5ea65b7
Ethan Skinner <10249884+el-ethan@users.noreply.github.com>
2022-11-06 21:44:20 -0600

    Update gatsby config for fonts

--- gatsby-config.js
+++ gatsby-config.js
@@ -31,6 +31,19 @@ module.exports = {
             },
         },
 
+        {
+            resolve: `gatsby-omni-font-loader`,
+            options: {
+                enableListener: true,
+                preconnect: [`https://rsms.me/`],
+                web: [
+                    {
+                        name: `Inter`,
+                        file: `https://rsms.me/inter/inter.css`,
+                    },
+                ],
+            },
+        },
         // this (optional) plugin enables Progressive Web App + Offline functionality
         // To learn more, visit: https://gatsby.dev/offline
         `gatsby-plugin-offline`,
